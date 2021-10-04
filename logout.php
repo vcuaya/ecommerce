0b8997fe059032899e1f4e57072ce6b429c98efc
Jared-Torres <jared.torres@alumno.buap.mx>
2021-10-04 10:02:38 -0500

    Update logout.php

---
+++ logout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="https://fonts.goggleapis.com/css/css?family=Roboto">
+	<link rel="stylesheet" type="text/css" href="assets/css/estilos1.css">
+</head>
+<body>
+
+	<?php require 'partials/header.php' ?>
+
+	<h1>BYE</h1>
+
+</body>
+</html>
