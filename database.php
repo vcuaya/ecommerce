d237777c23d71d29db52967c28475f0e2a30dfee
Jared-Torres <jared.torres@alumno.buap.mx>
2021-10-08 09:12:51 -0500

    Update database.php

--- database.php
+++ database.php
@@ -6,6 +6,7 @@
 
 	try{
 		$conn = new PDO("mysql:host=$server;dbname=$database;",$username,$password);
+		//$conn = mysqli_connect('localhost','root','','commerce');
 	} catch (PDOException $e){
 		die('Connected failed: '.$e->getMessage());
 	}
