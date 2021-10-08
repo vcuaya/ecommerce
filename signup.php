47196059bfd9e8c959e886dcdf01fdaaf01b5b8e
Jared-Torres <jared.torres@alumno.buap.mx>
2021-10-08 10:34:19 -0500

    borrando carpeta base de datos

--- signup.php
+++ signup.php
@@ -1,61 +1,64 @@
-<?php 
-	require 'database.php';
+<?php
 
-	$message = '';
+  require 'database.php';
 
-	if(!empty($_POST['user']) && !empty($_POST['correo']) && !empty($_POST['tel']) && !empty($_POST['nombre']) && !empty($_POST['paterno']) && !empty($_POST['pass'])){
-		
-		$sql = "INSERT INTO cliente (tel, correo, pass, user, nombre, paterno) VALUES (:tel, :correo, :pass, :user, :nombre, :paterno)";
+  $message = '';
 
-		$stmt = $conn->prepare($sql);
-		$stmt->bindParam(':tel',$_POST['tel']);
-		$correosinespacio = preg_replace('/\s+/', '', $_POST['correo']); 
+  if(!empty($_POST['user']) && !empty($_POST['correo']) && !empty($_POST['tel']) && !empty($_POST['nombre']) && !empty($_POST['paterno']) && !empty($_POST['password'])){
+    
+    $sql = "INSERT INTO cliente (tel, correo, password, user, nombre, paterno) VALUES (:tel, :correo, :password, :user, :nombre, :paterno)";
 
-		$stmt->bindParam(':correo',$correosinespacio);
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':tel',$_POST['tel']);
+    $correosinespacio = preg_replace('/\s+/', '', $_POST['correo']); 
 
-		$password = password_hash($_POST['pass'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':correo',$correosinespacio);
 
-		$stmt->bindParam(':pass',$password);
-		$stmt->bindParam(':user',$_POST['user']);
-		$stmt->bindParam(':nombre',$_POST['nombre']);
-		$stmt->bindParam(':paterno',$_POST['paterno']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-		if ($stmt->execute()){
-		$message = 'Cuenta creada con exito';
-		}else{
-		$message = 'Error al crear cuenta';
-		}
+    $stmt->bindParam(':password',$password);
+    $stmt->bindParam(':user',$_POST['user']);
+    $stmt->bindParam(':nombre',$_POST['nombre']);
+    $stmt->bindParam(':paterno',$_POST['paterno']);
 
-	}
+    if ($stmt->execute()){
+    $message = 'Cuenta creada con exito';
+    }else{
+    $message = 'Error al crear cuenta';
+    }
+
+  }
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Registrarse</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="https://fonts.goggleapis.com/css/css?family=Roboto">
-	<link rel="stylesheet" type="text/css" href="assets/css/estilos1.css">
-</head>
-<body>
-	<?php require 'partials/header.php' ?>
-	<?php if(!empty($message)): ?>
-		<p><?= $message ?></p>
-	<?php endif; ?>
+  <head>
+    <meta charset="utf-8">
+    <title>Registro</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilos1.css">
+  </head>
+  <body>
 
-	<h1>Resgistrarse</h1>
-	<span><a href="login.php">Iniciar sesion</a></span>
-	<form action="signup.php" method="post">
-		
-		<input type="text" name="correo" placeholder="Correo electronico">
-		<input type="text" name="user" placeholder="Nombre de usuario">
-		<input type="text" name="nombre" placeholder="Nombre">
-		<input type="text" name="paterno" placeholder="Apellido paterno"> 
-		<input type="text" name="tel" placeholder="Numero celular">
-		<input type="password" name="pass" placeholder="Contraseña">
-		<input type="password" name="passwordcon" placeholder="Confirmar contraseña">
-		<input type="submit" name="send">
+    <?php require 'partials/header.php' ?>
 
-	</form>
-</body>
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+
+  <h1>Resgistrarse</h1>
+  <span><a href="login.php">Iniciar sesion</a></span>
+  <form action="signup.php" method="post">
+    
+    <input type="text" name="correo" placeholder="Correo electronico">
+    <input type="text" name="user" placeholder="Nombre de usuario">
+    <input type="text" name="nombre" placeholder="Nombre">
+    <input type="text" name="paterno" placeholder="Apellido paterno"> 
+    <input type="text" name="tel" placeholder="Numero celular">
+    <input type="password" name="password" placeholder="Contraseña">
+    <input type="password" name="passwordcon" placeholder="Confirmar contraseña">
+    <input type="submit" name="send">
+
+  </form>
+
+  </body>
 </html>
