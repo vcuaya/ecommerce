cbd6d957edf6be82727e3dac2f541ee6ccfa6d2e
Jared-Torres <jared.torres@alumno.buap.mx>
2021-10-08 09:12:57 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,35 +1,50 @@
 <?php
-	session_start();
+session_start();
+$alert ='';
+require "database.php";
 
-	if(isset($_SESSION['user_id'])){
-		header('Location: /indice.php');
+if(!empty($_POST))
+{
+	if(empty($_POST['correo']) || empty($_POST['pass']))
+	{
+		$alert = 'Ingrese usuario y contraseña';
 	}
-	require 'database.php';
+	else{
+		
 
-	if(!empty($_POST['correo']) && !empty($_POST['password'])){
-		$records = $conn->prepare('SELECT idcliente, correo, password FROM cliente WHERE correo = :correo');
-		$records->bindParam(':correo', $_POST['correo']);
-		$records->execute();
-		$results = $records->fetch(PDO::FETCH_ASSOC);
+		$correo = $_POST['correo'];
+		$password = password_hash($_POST['pass'], PASSWORD_BCRYPT);
 
-		$message = '';
+		/*$query = $conn->prepare('SELECT idcliente, correo, pass FROM cliente WHERE correo = :correo and pass = :pass');
+		$query->bindParam(':correo', $_POST['correo']);
+		$query->bindParam(':pass', $password);
+		$query->execute();*/
 
-		if (($num = count($results)) > 0 &&  password_verify($_POST['password'], $results['password'])){
-			$message = "Exito";
-			$_SESSION['user_id'] = $results['idcliente'];
-			header("Location: /indice.php");
-		}else{
-			if(count($results) != 0)
-			{
-				echo "$num";
-				$message = "Estas credenciales no coinciden, intente de nuevo";
-			}else{
-				if(password_verify($_POST['password'], $results['password'])){
-					$message = "Contraseña incorrecta";
-				}
-			}
+		$query = mysqli_query($conn,"SELECT * FROM cliente WHERE correo = '$correo' and pass = '$password'");
+		//$result = 0;
+		//$result = count($query);
+		//$result = mysqli_num_rows($query);
+		$result = mysqli_num_rows($query);
+
+		
+		
+		
+		
+		if($result > 0)
+		{
+			$data = mysqli_fetch_array($query);
+			$_SESSION['correo'] = $data['correo'];
+			$_SESSION['pass'] = $data['pass'];
+
+			header('location:indice.php');
+		}
+		else
+		{
+			$alert= 'Usuario o contraseña inválidos';
+			session_destroy();
 		}
 	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -52,7 +67,7 @@
 
 	<form action="login.php" method="post">
 		<input type="text" name="correo" placeholder="Correo electronico">
-		<input type="password" name="password" placeholder="Contraseña">
+		<input type="password" name="pass" placeholder="Contraseña">
 		<input type="submit" name="send">
 	</form>
 	<span><a href="signup.php">Registrarse</a></span>
