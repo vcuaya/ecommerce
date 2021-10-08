c79edcfb97ac43682670621389f95b2afa0e06d4
Jared-Torres <jared.torres@alumno.buap.mx>
2021-10-08 09:12:54 -0500

    Update indice.php

--- indice.php
+++ indice.php
@@ -1,17 +1,44 @@
+<?php
+  session_start();
+
+  require 'database.php';
+
+  if (isset($_SESSION['user_id'])) {
+    $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
+    $records->bindParam(':id', $_SESSION['user_id']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $user = null;
+
+    if (count($results) > 0) {
+      $user = $results;
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Bienvenido a e-commerce</title>
-	<link rel="stylesheet" type="text/css" href="https://fonts.goggleapis.com/css/css?family=Roboto">
-	<link rel="stylesheet" type="text/css" href="assets/css/estilos1.css">
-</head>
-<body>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to you WebApp</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilos1.css">
+  </head>
+  <body>
+    <?php require 'partials/header.php' ?>
 
-	<?php require 'partials/header.php' ?>
+    <?php if(!empty($user)): ?>
+      <br> Welcome. <?= $user['correo']; ?>
+      <br>You are Successfully Logged In
+      <a href="logout.php">
+        Logout
+      </a>
+    <?php else: ?>
+      <h1>Please Login or SignUp</h1>
 
-	<h1>Registrese o inicie sesion</h1>
-	<a href="login.php">Iniciar sesion</a>
-	<a href="signup.php">Resgistrarse</a>
-</body>
+      <a href="login.php">Login</a> or
+      <a href="signup.php">SignUp</a>
+    <?php endif; ?>
+  </body>
 </html>
